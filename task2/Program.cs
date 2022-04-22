c55c12f787c43d9b7144f43e3f0204a991616d34
mike_karni <3923499@mail.ru>
2022-04-22 18:01:24 +0300

    решил вторую задачу

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+Console.Write("Введите число А  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int s = 0;
+while (number > 0)
+{
+ 
+    s = s + number % 10;
+    number = number /10 ;
+ 
+}
+ Console.WriteLine(s);
