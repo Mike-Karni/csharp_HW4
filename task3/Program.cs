e79aafadfdebc5de842209877aa97b9ba179f310
mike_karni <3923499@mail.ru>
2022-04-23 14:45:22 +0300

    Решил задачу 3!!!

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+Поправка: на семинаре дал добро на заволнение массива рандомными числами. 
+В оригинале нужно пользователю ввести строку - ряд чисел через запятую, 
+а программа превращает эту строку в полноценный массив и выводит. Рекомендую вторым вариантом)*/
+
+Console.WriteLine("Введите значения массива через запятую");
+
+string array = Console.ReadLine();
+
+
+int dlina = array.Length;
+int index = 0;
+
+while(index<array.Length)
+{
+    Console.Write(array[index]+ " ");
+    index+=2 ;
+}
+
+//Console.WriteLine(dlina);
+
+/*int[] array = new int [8];
+
+void FilArray(int[] collection)
+{ 
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] massiv)
+{
+      int count = massiv.Length;
+      int position = 0;
+      while (position<count)
+      {
+          Console.Write(massiv[position]);
+          position++;
+      }
+
+}
+FilArray(array);
+PrintArray(array);*/
