a30ccca5bf3df58131c61b715f6345a7c9594a8c
mike_karni <3923499@mail.ru>
2022-04-23 13:21:31 +0300

    bodaus
     with '#' will be ignored, and an empty message aborts the commit.

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,12 +2,26 @@
 452 -> 11
 82 -> 10
 9012 -> 12*/
-
-
 Console.Write("Введите число А  ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int s = 0;
+string number1 = Convert.ToString(number);
+int sum = 0;
+int index = 0;
+
+while (index<number1.Length)
+{
+    sum = sum + Convert.ToInt32(number1[index]);
+    index++;
+    Console.WriteLine(sum);
+    Console.WriteLine(number1[index]);
+}
+Console.WriteLine(sum);
+
+
+
+
+/*int s = 0;
 while (number > 0)
 {
  
@@ -15,4 +29,16 @@ while (number > 0)
     number = number /10 ;
  
 }
- Console.WriteLine(s);
+ Console.WriteLine(s);*/
+
+
+// 111111111
+/*int number = RandomInt(100,1000);
+Console.WriteLine(number);
+string stringNumber = Convert.ToString(number);
+Console.WriteLine($"{stringNumber[0]}{stringNumber[2]}");
+
+/*int RandomInt(int enclusiveMin,int exclusiveMax)
+{
+    return new Random().Next(enclusiveMin,exclusiveMax);
+}*/
